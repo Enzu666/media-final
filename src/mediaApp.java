bbfe32a11b8288a44e02f8509aa5b53746a5f578
ENZO CREPALDI PEIXOTO <25203666@CFP127ALUNO.LOCAL>
2025-09-22 14:21:47 -0300

    info. necessárias

--- src/mediaApp.java
+++ src/mediaApp.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class mediaApp {
+        public static void main(String[] args) {
+            System.out.println("Iniciando");
+
+            mediaFinal calculo = new mediaFinal();
+            calculo.obterDados();
+
+        }
 }
+
--- src/mediaFinal.java
+++ src/mediaFinal.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class mediaFinal {
+    String nomeDoAluno;
+    double nota1;
+    double nota2;
+    double nota3;
+    double nota4;
+    double mediaFinal;
+    double media;
+
 }
